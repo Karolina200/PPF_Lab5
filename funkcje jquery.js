27004d429bc487d5402ac3d9ba2134d1ca5fd3e1
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-12-12 15:07:52 +0100

    commit

--- funkcje jquery.js
+++ funkcje jquery.js
@@ -1,6 +1,5 @@
-$button.onclick=(function(){
-$("button").click(function(){
-    $("p").hide();
-});
-
-});
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });
