800bcc65d58aba5e04dc30ae5c2e648ce39df464
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-12-12 20:48:59 +0100

    commit

--- funkcje jquery.js
+++ funkcje jquery.js
@@ -2,55 +2,41 @@
   $(document).ready(function(){
     $("#button_01").click(function(){
       $("p").hide(500);
-    });
-  });
-  $(document).ready(function(){
+    })
     $("#button_02").click(function(){
       $("#p_03").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#button_03").click(function(){
-      $(".cp_01").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#button_04").click(function(){
-      $("*").hide();
-    });
-  });
-  $(document).ready(function(){
+  })
+  $("#button_03").click(function(){
+    $(".cp_01").hide();
+  })
+  $("#button_04").click(function(){
+    $("*").hide();
+  })
+  $("#button_04").click(function(){
+    $("*").hide();
+  })
+  $("#button_04").click(function(){
+    $("*").hide();
+  })
     $("#button_05").click(function(){
       $("[href]").hide();
-    });
-  });
-  $(document).ready(function(){
+    })
     $("#button_06").click(function(){
       $("h1").hide();
-    });
-  });
-  $(document).ready(function(){
+    })
     $("#button_07").click(function(){
       $("ul li:first").hide();
-    });
-  });
-  $(document).ready(function(){
+    })
     $("#button_08").click(function(){
       $("ul li:first-child").hide();
-    });
-  });
-  $(document).ready(function(){
+  })
     $("#button_09").click(function(){
       $(":button").hide();
-    });
-  });
-  $(document).click(function(){
+    })
     $("#button_10").click(function(){
-    $("p").toggle();
-  });
-});
-$(document).ready(function(){
+    $("p").show();
+  })
   $("#button_11").click(function(){
-    $(".cp_01").toggle();
-  });
+    $(".cp_02").toggle();
+  })
 });
