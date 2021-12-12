76cceac92c0201b037e0e061f7e84fa3c81a8b4e
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-12-12 12:35:39 +0100

    commit

--- funkcje_jquery.js
+++ funkcje_jquery.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function () {
+    $("button").click(function () {
         $("p").hide();
     });
+    
     });
