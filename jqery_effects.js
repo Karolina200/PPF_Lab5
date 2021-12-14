17de1c40e3eb61a7cc7ec41872a3652ad1316451
Karolina200 <karolina.ploszaj@student.pk.edu.pl>
2021-12-14 14:30:11 +0100

    commit

--- jqery_effects.js
+++ jqery_effects.js
@@ -3,31 +3,31 @@ $(document).ready(function () {
     $("#p1").fadeOut("fast");
     $("#p2").fadeOut("slow");
     $("#p3").fadeOut(800);
-  });
+  })
   $("#button_02").click(function () {
     $("#p1").fadeIn("fast");
     $("#p2").fadeIn("slow");
     $("#p3").fadeIn(800);
-  });
+  })
   $("#button_03").click(function () {
     $("#p1").fadeTo("fast", 0.1);
     $("#p2").fadeTo("slow", 0.7);
     $("#p3").fadeTo(800, 0.5);
-  });
+  })
   $("#button_04").click(function () {
     $("#p1").fadeToggle("fast");
     $("#p2").fadeToggle("slow");
     $("#p3").fadeToggle(800);
-  });
+  })
   $("#button_05").click(function () {
     $("p1").slideDown();
-  });
+  })
   $("#button_06").click(function () {
     $("p1").slideUp();
-  });
+  })
   $("#button_07").click(function () {
     $("p1").slideToggle();
-  });
+  })
   $("#button_08").click(function () {
     var div = $(".redSquare");
     div.animate({ height: "500px", fontSize: "0.7em" }, "slow");
@@ -40,20 +40,20 @@ $(document).ready(function () {
     div.css("background-color", "blue");
     div.css("color", "red");
     div.animate({ width: "100px" }, "slow");
-  });
+  })
   $("#button_09").click(function () {
     $(".redSquare").stop(true, true);
-  });
+  })
   $("#button_10").click(function () {
     $(".redSquare").hide("slow", function () {
       alert("Kwadrat ukryto");
-    });
-  });
+    })
+  })
   $("#button_11").click(function () {
     $(".redSquare").css("color", "green").slideUp(1000).slideDown(1000);
   });
   $("#button_12").click(function () {
   $(".redSquare").css("color", "purple").css("background","black")
   .slideToggle(4000);
-});
+})
 });
